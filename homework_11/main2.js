e55e4f5ca94d5f03ff9042b41539c613c30a41e3
Vikky <vikky@iMac-Vikky.local>
2021-05-11 11:15:44 +0300

    add hm11

---
+++ homework_11/main2.js
@@ -0,0 +1,11 @@
+//task 2
+const personaleModule = require("./personalmodule.js");
+const os = require("os");
+const http = require("http");
+
+const requestListener = function (request, responce) {
+  responce.end(personaleModule.greeting(os.userInfo().username));
+};
+
+const server = http.createServer(requestListener);
+server.listen(5000);
